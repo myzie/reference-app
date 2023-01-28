9ad1d2ce21285f436ccda400f0f8027914cbc942
Curtis Myzie <curtis.myzie@gmail.com>
2023-01-28 09:35:00 -0500

    Request databases from backend

--- backend/main.go
+++ backend/main.go
@@ -24,8 +24,25 @@ func main() {
 	e.Use(middleware.RequestID())
 	e.Use(lecho.Middleware(lecho.Config{Logger: echoLogger}))
 
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:3000", "http://localhost:8080"},
+		AllowHeaders: []string{"*"},
+	}))
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.GET("/databases", func(c echo.Context) error {
+		type db struct {
+			Name    string `json:"name"`
+			Version string `json:"version"`
+			Engine  string `json:"engine"`
+		}
+		var dbs []db
+		dbs = append(dbs, db{Name: "db1", Version: "1.0", Engine: "mysql"})
+		dbs = append(dbs, db{Name: "db2", Version: "2.0", Engine: "postgres"})
+		return c.JSON(http.StatusOK, dbs)
 	})
 
 	logger.Info().Msg("starting server")
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
 import ProTip from './ProTip';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import axios from 'axios';
 
 function Copyright() {
   return (
@@ -18,14 +26,60 @@ function Copyright() {
   );
 }
 
+function DatabaseTable() {
+  const [rows, setRows] = useState([]);
+  // let rows = [
+  //   { name: "Development" },
+  //   { name: "Staging" },
+  //   { name: "Production" }
+  // ]
+
+  useEffect(() => {
+    async function fetchData() {
+      const result = await axios('http://localhost:1323/databases');
+      setRows(result.data);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Database</TableCell>
+            <TableCell align="right">Version</TableCell>
+            <TableCell align="right">Engine</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.version}</TableCell>
+              <TableCell align="right">{row.engine}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
 export default function App() {
   return (
-    <Container maxWidth="sm">
+    <Container maxWidth="md">
       <Box sx={{ my: 4 }}>
         <Typography variant="h4" component="h1" gutterBottom>
           Reference App
         </Typography>
         <ProTip />
+        <DatabaseTable />
         <Copyright />
       </Box>
     </Container>
